6f4d2907a940153f1a194058eec47ba604262e47
Timo Pomer <timopomer@gmail.com>
2022-02-12 14:16:20 +0200

    save

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="mlx90614-prometheus",
     version="0.0.1",
     author="Timo Pomer",
     author_email="timopomer@gmail.com",
     description="Expose mlx90614 sensor data as a prometheus endpoint",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/timopomer/mlx90614-prometheus",
     project_urls={
         "Bug Tracker": "https://github.com/timopomer/mlx90614-prometheus/issues",
